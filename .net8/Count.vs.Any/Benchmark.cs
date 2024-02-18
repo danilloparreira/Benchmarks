53f3689e73f0bf1fc053172be8b4076358a35dd0
Danillo parreira <danillo.aug.uezo@gmail.com>
2024-02-18 13:38:17 -0300

    Count vs Any()

---
+++ .net8/Count.vs.Any/Benchmark.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using Bogus;
+
+namespace Count.vs.Any;
+
+[RankColumn]
+[MemoryDiagnoser]
+public class Benchmark
+{
+    private readonly int numberItems = 1000;
+    private readonly Faker faker = new();
+
+    [Benchmark]
+    public bool CountListItems()
+    {
+        var list = new List<string>();
+
+        for (int i = 0; i < numberItems; i++)
+        {
+            list.Add(faker.Lorem.Word());
+        }
+
+        var count = list.Count > 0;
+
+        return count;
+    }
+
+    [Benchmark]
+    public bool AnyListItems()
+    {
+        var list = new List<string>();
+
+        for (int i = 0; i < numberItems; i++)
+        {
+            list.Add(faker.Lorem.Word());
+        }
+
+        var any = list.Any();
+
+        return any;
+    }
+}
