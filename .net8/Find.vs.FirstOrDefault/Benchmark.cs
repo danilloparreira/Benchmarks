5689d35603e8eab634d81fa5c092e6c98b24b996
Danillo parreira <danillo.aug.uezo@gmail.com>
2024-02-27 10:46:45 -0300

    Find vs FirstOrDefault

---
+++ .net8/Find.vs.FirstOrDefault/Benchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using Bogus;
+
+namespace Find.vs.FirstOrDefault;
+
+[RankColumn]
+[MemoryDiagnoser]
+public class Benchmark
+{
+    private readonly List<string> stringList = [];
+    private readonly int numberItems = 1000;
+    private readonly Faker faker = new();
+
+    public Benchmark()
+    {
+        for (int i = 0; i < numberItems; i++)
+        {
+            stringList.Add(faker.Lorem.Letter());
+        }
+    }
+
+    [Benchmark]
+    public string FindItem()
+    {
+        var item = "a";
+
+        var itemFound = stringList.Find(x => x == item);
+
+        return itemFound;
+    }
+
+    [Benchmark]
+    public string FirstOrDefaultItem()
+    {
+        var item = "a";
+
+        var itemFound = stringList.FirstOrDefault(x => x == item);
+
+        return itemFound;
+    }
+}
---
+++ .net8/Find.vs.FirstOrDefault/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Find.vs.FirstOrDefault;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmark>(config: null, args: args);
+        Console.ReadLine();
+    }
+}
